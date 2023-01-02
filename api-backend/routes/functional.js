2356195c2c04fc7c5c7403e01ab6f0d96620e51a
Dimitrios Georgousis <el19005@mail.ntua.gr>
2023-01-02 14:19:41 +0200

    Created functionality endpoints

--- api-backend/routes/functional.js
+++ api-backend/routes/functional.js
@@ -1,17 +1,31 @@
+// this module contains all the functionality endpoints
+
 const express = require("express");
-const hsc = require("http-status-codes");
+const hsc = require("http-status-codes"); // quick way to access status codes
 const router = express.Router();
 
+// fetch a questionnaire from the database
+router.get("/questionnaire/:questionnaireID", (req, res) => {
+    res.status(hsc.StatusCodes.OK).send(hsc.ReasonPhrases.OK);
+});
+ // fetch a question from the database
 router.get("/question/:questionnaireID/:questionID", (req, res) => {
-    res.send(hsc.ReasonPhrases.OK);
+    res.status(hsc.StatusCodes.OK).send(hsc.ReasonPhrases.OK);
 });
 
-router.post("/doanswer/:questionnaireID/:questionID/:session/:optionID", (req, res) => {
-    res.send(hsc.ReasonPhrases.OK);
+// post an answer to the database
+router.post("/doanswer/:questionnaireID/:questionID/:sessionID/:optionID", (req, res) => {
+    res.status(hsc.StatusCodes.OK).send(hsc.ReasonPhrases.OK);
 });
 
-router.post("/getsessionanswers/:questionnaireID/:session", (req, res) => {
-    res.send(hsc.ReasonPhrases.OK);
+// fetch a session from the database
+router.get("/getsessionanswers/:questionnaireID/:sessionID", (req, res) => {
+    res.status(hsc.StatusCodes.OK).send(hsc.ReasonPhrases.OK);
+});
+
+// fetch all the answers to a question from the database
+router.get("/getquestionanswers/:questionnaireID/:questionID", (req, res) => {
+    res.status(hsc.StatusCodes.OK).send(hsc.ReasonPhrases.OK);
 });
 
 module.exports = router;
