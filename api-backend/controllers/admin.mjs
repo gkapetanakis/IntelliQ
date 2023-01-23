7d0c1c5780604375affb4859b2809695e0a2b2e2
Dimitrios Georgousis <el19005@mail.ntua.gr>
2023-01-23 21:01:02 +0200

    implemented post questionnaire endpoint

--- api-backend/controllers/admin.mjs
+++ api-backend/controllers/admin.mjs
@@ -17,8 +17,41 @@ function getHealthcheck(req, res) {
 }
 
 // upload a questionnaire to the database
-function postQuestionnaireUpd(req, res) {
-    // TODO
+async function postQuestionnaireUpd(req, res) {
+    let statusCode = StatusCodes.NO_CONTENT;
+    let reasonValue = undefined;
+    let response = undefined;
+
+    // turn the uploaded document into a JSON object
+    // req.file.buffer contains the uploaded file data as bytes
+    // toString() is required to make it readable
+    // JSON.parse() turns a JSON string into a proper JSON object
+    const jsonified = JSON.parse(req.file.buffer.toString());
+    // create a questionnaire with the given values
+    const questionnaire = new Questionnaire(jsonified);
+
+    // save the questionnaire
+    const { success } = await saveDocument(questionnaire);
+    if (!success) {
+        console.log(error);
+        statusCode = StatusCodes.BAD_REQUEST;
+        reasonValue = "Bad questionnaire parameters"; // When proper validation is added, this will become more specific
+        response = { error: reasonValue };
+    }
+    res.status(statusCode).send(response);
+
+    // helper functions --------------------
+
+    async function saveDocument(document) {
+        try {
+            await document.save();
+            console.log("A new questionnaire was saved to the database");
+            return { success: true };
+        } catch (error) {
+            console.log("Failed to save a new questionnaire to the database:", error);
+            return { success: false };
+        }
+    }
 }
 
 // delete all documents form the database
