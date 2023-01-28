c68c044db20c2fc4d64a327effae394d056c31cf
Dimitrios Georgousis <el19005@mail.ntua.gr>
2023-01-28 14:03:08 +0200

    Implemented postResetAll and postResetQ for admin.

--- api-backend/controllers/admin.mjs
+++ api-backend/controllers/admin.mjs
@@ -55,13 +55,109 @@ async function postQuestionnaireUpd(req, res) {
 }
 
 // delete all documents form the database
-function postResetAll(req, res) {
-    // TODO
+async function postResetAll(req, res) {
+    let statusCode = StatusCodes.OK;
+    let response = {
+        status: "OK",
+        reason: undefined
+    };
+
+    const { qSuccess } = await deleteAllQuestionnaires();
+    const { sSuccess } = await deleteAllSessions();
+
+    if (!qSuccess || !sSuccess) {
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+        response.status = "failed";
+        response.reason = "An error occured while querying the database, ";
+    }
+
+    if (!qSuccess && !sSuccess) response.reason += "no documents were deleted";
+    else if (!qSuccess) response.reason += "only sessions were deleted";
+    else if (!sSuccess) response.reason += "only questionnaires were deleted";
+
+    res.status(statusCode).json(response);
+
+    // helper functions --------------------
+
+    async function deleteAllQuestionnaires() {
+        try {
+            const { deletedCount } = await Questionnaire.deleteMany();
+            console.log(`Deleted all ${deletedCount} questionnaires from the database`);
+            return { success: true };
+        } catch (error) {
+            console.error("An error occured while trying to delete all questionnaires:", error);
+            return { success: false };
+        }
+    }
+
+    async function deleteAllSessions() {
+        try {
+            const { deletedCount } = await Session.deleteMany();
+            console.log(`Deleted all ${deletedCount} sessions from the database`);
+            return { success: true };
+        } catch (error) {
+            console.error("An error occured while trying to delete all sessions:", error);
+            return { success: false };
+        }
+    }
 }
 
 // delete all sessions of a given questionnaire
-function postResetQ(req, res) {
-    // TODO
+async function postResetQ(req, res) {
+    let statusCode = StatusCodes.OK;
+    let response = {
+        status: "OK",
+        reason: undefined
+    };
+
+    const { vSuccess, vError } = await verifyQuestionnaireId();
+    if (vError) {
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+        response.status = "failed";
+        response.reason = "An error occured while querying the database";
+    } else if (!vSuccess) {
+        statusCode = StatusCodes.NOT_FOUND;
+        response.status = "failed";
+        response.reason = "No questionnaire exists with the given ID";
+    } else {
+        const { dSuccess } = await deleteQuestionnaireSessions();
+        if (!dSuccess) {
+            statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+            response.status = "failed";
+            response.reason = "An error occured while querying the database";
+        }
+    }
+
+    res.status(statusCode).json(response);
+
+    // helper functions --------------------
+
+    async function verifyQuestionnaireId(questionnaireID) {
+        try {
+            const foundQuestionnaire = await Questionnaire.findOne().where("questionnaireID").equals(questionnaireID);
+            return {
+                success: foundQuestionnaire !== null,
+                error: false
+            };
+        } catch (error) {
+            console.error("An error occured while trying to find the given questionnaire:", error);
+            return {
+                success: false,
+                error: true
+            };
+        }
+    }
+
+    async function deleteQuestionnaireSessions(questionnaireID) {
+        try {
+            const { deletedCount } = await Session.deleteMany(queryOptions).where("questionnaireID").equals(questionnaireID);
+            console.log(`Deleted ${deletedCount} sessions from the database`);
+            return { success: true };
+        } catch (error) {
+            console.error("An error occured while trying to delete the requested sessions:", error);
+            return { success: false };
+        }
+    }
 }
 
 export { getHealthcheck, postQuestionnaireUpd, postResetAll, postResetQ };
