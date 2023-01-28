943bd34392ccbac3f1d5b7097bf4033ea92b62ae
Georgios Alexios Kapetanakis <george.kapetanakis.gk@gmail.com>
2023-01-28 14:53:33 +0200

    Implemented query helper function

--- api-backend/lib/queryUtils.mjs
+++ api-backend/lib/queryUtils.mjs
@@ -1,19 +1,23 @@
-async function findQueryHelper(query) {
-    // UNFINISHED
+// executes the given query and returns a pair { res, err }
+// res is the query response and it is:
+//      the document(s) found in a "find" query
+//      the number of documents deleted in a "delete" query
+//      null if an error occured
+// err is:
+//      null if no error occured
+//      the error that occured if an error occured
+// this function basically just wraps a try/catch block that would otherwise be repeated everywhere
+async function executeQuery(query) {
+    let res = null;
+    let err = null;
     try {
-        const { doc, err } = await query;
-    } catch (err) {
-        handleErrors(err);
+        res = await query;
+        console.debug("A query completed successfully");
+    } catch (error) {
+        err = error;
+        console.debug("An error occured while executing a query");
     }
+    return { res, err };
 }
 
-async function deleteQueryHelper(query) {
-    // UNFINISHED
-    try {
-        const { cnt } = await query;
-    } catch (err) {
-        handleErrors(err);
-    }
-}
-
-export { findQueryHelper, deleteQueryHelper };
+export { executeQuery };
