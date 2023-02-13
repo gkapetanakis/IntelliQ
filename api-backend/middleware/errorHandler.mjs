4a04b51c09e663d345673f7acd120ba550fbe936
Georgios Alexios Kapetanakis <george.kapetanakis.gk@gmail.com>
2023-02-13 19:37:38 +0200

    Some basic error messages to the user

--- api-backend/middleware/errorHandler.mjs
+++ api-backend/middleware/errorHandler.mjs
@@ -1,6 +1,16 @@
+import { StatusCodes, ReasonPhrases } from "http-status-codes";
+
 function errorHandler(err, req, res, next) {
-    console.error(err);
-    res.send("Error Lmao");
+    const error = { message: "An error occured" };
+    if (res.statusCode === StatusCodes.BAD_REQUEST)
+        error.message = ReasonPhrases.BAD_REQUEST;
+    else if (res.statusCode === StatusCodes.NOT_FOUND)
+        error.message = ReasonPhrases.NOT_FOUND;
+    else if (res.statusCode === StatusCodes.INTERNAL_SERVER_ERROR)
+        error.message = ReasonPhrases.INTERNAL_SERVER_ERROR;
+    else
+        console.error("Unknown error status code encountered:", res.statusCode);
+    res.json(error);
 }
 
 export default errorHandler;
