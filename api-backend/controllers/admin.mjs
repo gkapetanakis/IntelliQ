b37517840faa6a1537d80bcd26595201434c87f2
Georgios Alexios Kapetanakis <george.kapetanakis.gk@gmail.com>
2023-02-14 16:57:44 +0200

    Even more logging improvements plus some cleanup

--- api-backend/controllers/admin.mjs
+++ api-backend/controllers/admin.mjs
@@ -22,16 +22,22 @@ function getHealthcheck(_req, res, next) {
 
 // upload a questionnaire to the database
 async function postQuestionnaireUpd(req, res, next) {
-    res.locals.model = Questionnaire;
     // turn the uploaded document into a JSON object:
     // req.file.buffer contains the uploaded file data as bytes
     // toString() is required to make it readable
     // JSON.parse() turns a json string into a proper json object
     try {
+        // throws error if file does not exist
         res.locals.obj = JSON.parse(req.file.buffer.toString());
+        res.locals.model = Questionnaire;
+        res.status(StatusCodes.OK);
         next();
     } catch (err) {
-        res.status(StatusCodes.BAD_REQUEST);
+        // file did not exist
+        res.status(err instanceof TypeError || err instanceof mongoose.Error.ValidationError
+            ? StatusCodes.BAD_REQUEST
+            : StatusCodes.INTERNAL_SERVER_ERROR
+        );
         next(err);
     }
 }
@@ -40,6 +46,9 @@ async function postQuestionnaireUpd(req, res, next) {
 // will delete questionnaires only if the answers are successfully deleted
 // in order to not leave the database in an inconsistent state
 async function postResetAll(_req, res, next) {
+    res.status(StatusCodes.OK);
+    res.locals.responseObj = { status: "OK" };
+
     try {
         await Answer.deleteMany();
     } catch {
@@ -49,29 +58,36 @@ async function postResetAll(_req, res, next) {
             reason: "Nor answers, neither questionnaires could be purged"
         };
         next();
+        return;
     }
 
     try {
         await Questionnaire.deleteMany();
-        res.status(StatusCodes.OK);
-        res.locals.responseObj = { status: "OK" };
     } catch {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR);
         res.locals.responseObj = {
             status: "failed",
             reason: "Answers purged; questionnaires could not be purged"
         };
-    } finally {
-        next();
     }
+
+    // this endpoint handles its own errors,
+    // therefore it never needs to call the error handler
+    next();
 }
 
 // delete all sessions of a given questionnaire
 async function postResetQ(req, res, next) {
     const questionnaireID = req.params.questionnaireID;
+    res.status(StatusCodes.OK);
+    res.locals.responseObj = { status: "OK" };
 
     try {
-        const questionnaire = await Questionnaire.exists().where({ "questionnaireID" : questionnaireID }).lean();
+        const questionnaire = await Questionnaire
+            .exists()
+            .where({ "questionnaireID" : questionnaireID })
+            .lean();
+
         if (!questionnaire) {
             res.status(StatusCodes.BAD_REQUEST);
             res.locals.responseObj = {
@@ -79,9 +95,8 @@ async function postResetQ(req, res, next) {
                 reason: "Unable to find a questionnaire with the given ID"
             };
         } else {
-            await Answer.deleteMany().where({ "questionnaireID" : questionnaireID });
-            res.status(StatusCodes.OK);
-            res.locals.responseObj = { status: "OK" };
+            await Answer.deleteMany()
+                .where({ "questionnaireID" : questionnaireID });
         }
     } catch {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR);
@@ -89,9 +104,11 @@ async function postResetQ(req, res, next) {
             status: "failed",
             reason: "Internal server error"
         };
-    } finally {
-        next();
     }
+    
+    // this endpoint handles its own errors,
+    // therefore it never needs to call the error handler
+    next();
 }
 
 export {
